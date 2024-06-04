f864ea08ed68b8888d895f1801557342f246aef2
Jorge Luis Prieto Diaz <pdjorgeluis@gmail.com>
2024-06-04 17:54:01 -0400

    ya

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ const unknownEndPoint = (request, response) => {
     response.status(404).send({error: 'unknown endpoint'})
 }
   
-const PORT = process.env.PORT || 10000
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
